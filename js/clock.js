9fb87328d757f69cf6bbe1961595a219b343e2ed
Aditya Kumar <itsadityakr27@gmail.com>
2024-05-29 22:15:16 +0530

    updated

--- js/clock.js
+++ js/clock.js
@@ -2,13 +2,18 @@ function updateTime() {
   var today = new Date();
   var hours = today.getHours();
   var minutes = today.getMinutes();
-  var ampm = hours >= 12 ? 'PM' : 'AM';
+  var ampm = hours >= 12 ? 'PM': 'AM';
   hours = hours % 12;
-  hours = hours ? hours : 12; // Convert 0 to 12
-  minutes = minutes < 10 ? '0' + minutes : minutes;
+  hours = hours ? hours: 12; // Convert 0 to 12
+  minutes = minutes < 10 ? '0' + minutes: minutes;
   var timeString = hours + ':' + minutes + ' ' + ampm;
 
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  var options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+  };
   var dateString = today.toLocaleDateString(undefined, options);
 
   var timeDiv = document.getElementById('time');
